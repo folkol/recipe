3493a06f05e926fe15a8886c0b3ee0eaa829fe89
Mattias Johansson <mattias.johansson@atex.com>
2014-08-23 01:45:52 +0200

    Full coverage of RecipeService

--- src/RecipeServiceTest.java
+++ src/RecipeServiceTest.java
@@ -1,10 +1,12 @@
 import static org.mockito.Mockito.*;
 
 import java.util.Random;
+import java.util.UUID;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -49,6 +51,34 @@ public class RecipeServiceTest {
         service.findAll();
 
         verify(tq, times(1)).getResultList();
+    }
+
+    @Test
+    public void shouldDeleteRecipe() {
+        int i = new Random().nextInt();
+        Recipe recipe = new Recipe();
+        when(em.find(Recipe.class, i)).thenReturn(recipe);
+
+        service.delete(i);
+
+        verify(em, times(1)).find(Recipe.class, i);
+        verify(em, times(1)).remove(recipe);
+    }
+
+    @Test
+    public void shouldUpdateRecipe() {
+        Recipe oldRecipe = new Recipe();
+        int i = new Random().nextInt();
+        Recipe newRecipe = new Recipe();
+        newRecipe.title = UUID.randomUUID().toString();
+        newRecipe.description = UUID.randomUUID().toString();
+
+        when(em.find(Recipe.class, i)).thenReturn(oldRecipe);
+
+        service.update(newRecipe, i);
+
+        Assert.assertEquals("Title not updated", oldRecipe.title, newRecipe.title);
+        Assert.assertEquals("Description not updated", oldRecipe.description, newRecipe.description);
     }
 
 }
